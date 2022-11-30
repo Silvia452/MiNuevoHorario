e55b231c111f7a81cdeebb891362f632f60a1a40
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2022-11-30 09:24:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/minuevohorario/Date.java
@@ -0,0 +1,22 @@
+package com.example.minuevohorario;
+
+public class Date {
+
+    public static int diaOrdinal(String dia) {
+
+        switch (dia) {
+            case "Lunes":
+                return 2;
+            case "Martes":
+                return 3;
+            case "Miércoles":
+                return 4;
+            case "Jueves":
+                return 5;
+            case "Viernes":
+                return 6;
+            default:
+                return -1;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/minuevohorario/NuevaClase.java
@@ -0,0 +1,4 @@
+package com.example.minuevohorario;
+
+public class NuevaClase {
+}
